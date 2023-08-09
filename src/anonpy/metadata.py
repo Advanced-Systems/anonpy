e8636676fae9a87af370a4ccf8ee844de4cdaf63
StefanGreve <greve.stefan@outlook.jp>
2023-08-09 16:12:21 +0300

    Create meta data file

---
+++ src/anonpy/metadata.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from importlib.metadata import metadata
+
+from .internals import ConfigHandler, unique
+
+__meta_data = metadata("anonpy")
+__package__ = __meta_data["Name"]
+__version__ = __meta_data["Version"]
+__license__ = __meta_data["License"]
+__credits__ = unique([
+    __meta_data["Author"],
+    __meta_data["Maintainer"],
+])
