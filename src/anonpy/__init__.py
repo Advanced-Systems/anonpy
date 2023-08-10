630e3c8296112eeec45f497c919cad4796a8ef69
StefanGreve <greve.stefan@outlook.jp>
2023-08-10 13:18:21 +0300

    Export ServerResponse and StatusCode

--- src/anonpy/__init__.py
+++ src/anonpy/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 from .anonpy import AnonPy
+from .server_response import ServerResponse
+from .status_codes import StatusCode
 from .metadata import (
     __package__,
     __version__,
