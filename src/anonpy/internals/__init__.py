d81ac31f32612ae6d19b13ecee98c1daeac1e856
StefanGreve <greve.stefan@outlook.jp>
2023-08-11 18:18:07 +0300

    Improve code style and implement str2bool, readfile

--- src/anonpy/internals/__init__.py
+++ src/anonpy/internals/__init__.py
@@ -3,4 +3,4 @@
 from .config_handler import ConfigHandler
 from .log_handler import LogHandler
 from .request_handler import RequestHandler
-from .utils import _callback, _progressbar_options, unique
+from .utils import _callback, _progressbar_options, read_file, str2bool, unique
--- src/anonpy/internals/utils.py
+++ src/anonpy/internals/utils.py
@@ -1,38 +1,58 @@
 #!/usr/bin/env python3
 
-from typing import Any, List, Dict, Iterable
-from tqdm import tqdm
-from requests_toolbelt import MultipartEncoderMonitor
+from pathlib import Path
+from typing import Any, Dict, Iterable, Iterator, Union
 
-def unique(list_: List[Any]) -> List[Any]:
+from requests_toolbelt import MultipartEncoderMonitor
+from tqdm import tqdm
+
+
+def unique(iter: Iterable[Any]) -> Iterable[Any]:
     """
-    Remove all duplicated entries from a list.
+    Remove all duplicated entries from a collection.
     """
-    return list(dict.fromkeys(list_))
+    return list(dict.fromkeys(iter))
+
+def str2bool(val: str) -> bool:
+    """
+    Convert a string to boolean.
+
+    Note: This function only supports short and simple English responses to
+    yes/no questions.
+    """
+    return val.lower() in ("yes", "y", "true", "t", "1", "on", "")
+
+def read_file(path: Union[str, Path]) -> Iterator[str]:
+    """
+    Open a text file and returns its right-striped content line by line, except
+    those lines that start with a `#` character (comments).
+    """
+    with open(path, mode="r", encoding="utf-8") as file_handler:
+        yield (line.rstrip() for line in file_handler.readlines() if line[0] != "#")
 
 def _progressbar_options(
-            iterable: Iterable,
-            desc: str,
-            unit: str,
-            color: str="\033[32m",
-            char: str='\u25CB',
-            total: int=None,
-            disable: bool=False
-        ) -> Dict:
-        """
-        Return custom optional arguments for `tqdm` progressbars.
-        """
-        return {
-            'iterable': iterable,
-            'bar_format': "{l_bar}%s{bar}%s{r_bar}" % (color, "\033[0m"),
-            'ascii': char.rjust(9, ' '),
-            'desc': desc,
-            'unit': unit.rjust(1, ' '),
-            'unit_scale': True,
-            'unit_divisor': 1024,
-            'total': len(iterable) if total is None else total,
-            'disable': not disable
-        }
+        iterable: Iterable,
+        desc: str,
+        unit: str,
+        color: str="\033[32m",
+        char: str="\u25CB",
+        total: int=None,
+        disable: bool=False
+    ) -> Dict:
+    """
+    Return custom optional arguments for `tqdm` progressbars.
+    """
+    return {
+        "iterable": iterable,
+        "bar_format": "{l_bar}%s{bar}%s{r_bar}" % (color, "\033[0m"),
+        "ascii": char.rjust(9, " "),
+        "desc": desc,
+        "unit": unit.rjust(1, " "),
+        "unit_scale": True,
+        "unit_divisor": 1024,
+        "total": len(iterable) if total is None else total,
+        "disable": not disable
+    }
 
 def _callback(monitor: MultipartEncoderMonitor, tqdm_handler: tqdm) -> None:
     """
