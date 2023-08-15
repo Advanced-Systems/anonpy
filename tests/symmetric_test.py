6741acd917da4acf7fd6c4bb8a289710ba8b3198
StefanGreve <greve.stefan@outlook.jp>
2023-08-15 14:59:27 +0300

    Implement unit tests for Symmetric class

---
+++ tests/symmetric_test.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+from typing import Self
+
+import pytest
+
+from anonpy.security import KDF, Symmetric, generate_random_password
+
+
+@pytest.mark.parametrize("password", [None, generate_random_password(32)])
+@pytest.mark.parametrize("kdf", [KDF.PBKDF2HMAC, KDF.Scrypt])
+class TestSymmetric:
+    def test_encryption_and_decryption(self: Self, password: str, kdf: KDF) -> None:
+        # Arrange
+        sym = Symmetric()
+        data = "Hello, World!"
+
+        # Act
+        sym.generate_key(password, key_derivation_function=kdf)
+        cypher = sym.encrypt(data)
+        source = sym.decrypt(cypher)
+
+        # Assert
+        assert data == source
+
+    def test_encryption_and_decryption_with_key_file(self: Self, password: str, kdf: KDF) -> None:
+        # Arrange
+        sym = Symmetric(key_storage_path=Path.home())
+        data = "Hello, World!"
+        file = "test.key"
+
+        # Act
+        sym.generate_key(password, key_derivation_function=kdf)
+        sym.store_key(file)
+        sym.delete_key()
+        sym.read_key(file)
+
+        cypher = sym.encrypt(data)
+        source = sym.decrypt(cypher)
+
+        # Assert
+        assert data == source
