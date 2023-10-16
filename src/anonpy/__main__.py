9b2b5d7b7ccf59fbf42aed4445a7d479091e9c8b
StefanGreve <greve.stefan@outlook.jp>
2023-10-16 22:45:09 +0200

    Implement eval_config
    * rename init_settings method to init_config
    * implement eval_config
    * pass settings dictionary to commands instead of the config handler
      object
    
    A note on terminology: in this context, we use the name config to
    highlight the fact that the variable denotes a ConfigHandler object,
    while settings is a combination of these config values and the passed
    command line arguments (args).
    
    The eval_config method was implemented to move this logic to a single
    place, thus reducing the likelihood that sometime in the future an
    error will be made here due to prior code repetition.

--- src/anonpy/__main__.py
+++ src/anonpy/__main__.py
@@ -5,6 +5,7 @@ import json
 import subprocess
 from argparse import ArgumentParser, Namespace
 from pathlib import Path
+from typing import Any, Dict, Optional
 
 from colorama import Fore, Style, deinit, just_fix_windows_console
 from requests.exceptions import HTTPError
@@ -18,6 +19,8 @@ from .security import MD5, Checksum
 
 #region helpers
 
+type EvalConfig = Dict[str, Dict[str, Optional[Any]]]
+
 def print_diff(a: str, b: str) -> None:
         diff = difflib.ndiff(
             f"{a}\n".splitlines(keepends=True),
@@ -29,7 +32,10 @@ def print_diff(a: str, b: str) -> None:
 def copy_to_clipboard(text: str) -> None:
     subprocess.run("clip", input=text, check=True, encoding="utf-8")
 
-def init_settings(cfg_path: Path) -> None:
+def init_config(cfg_path: Path) -> None:
+    """
+    Create a new configuration file by force, potentially overwriting existing data.
+    """
     with ConfigHandler(cfg_path) as config_handler:
         config_handler.add_section("client", settings={
             "download_directory": Path("~/downloads").expanduser(),
@@ -49,38 +55,67 @@ def init_settings(cfg_path: Path) -> None:
             "preview": "/file/{}/info"
         })
 
+def eval_config(args: Namespace, config: ConfigHandler) -> EvalConfig:
+    """
+    Return a dictionary of config values, giving preference to command line arguments.
+    Some values have a fallback in case neither `args` nor `config` provide a value,
+    if `None` is not acceptable.
+    """
+    settings = {
+        "client": {
+            "download_directory": Path(getattr(args, "path", config.get_option("client", "download_directory", default=Path.cwd()))),
+            "token": getattr(args, "token", config.get_option("client", "token")),
+            "user_agent": getattr(args, "user_agent", config.get_option("client", "user_agent", default=RequestHandler.build_user_agent(__package__, __version__))),
+            "proxies": getattr(args, "proxies", config.get_option("client", "proxies")),
+            "enable_logging": getattr(args, "logging", config.get_option("client", "enable_logging")),
+            "log_level": config.get_option("client", "log_level"),
+            "verbose": getattr(args, "verbose", config.get_option("client", "verbose")),
+            "force": getattr(args, "force", config.get_option("client", "force")),
+        },
+        "server": {
+            "api": config.get_option("server", "api"),
+            "upload": config.get_option("server", "upload"),
+            "download": config.get_option("server", "download"),
+            "preview": config.get_option("server", "preview"),
+        }
+    }
+
+    settings["object"] = {
+        "endpoint": Endpoint(settings["server"]["upload"], settings["server"]["download"], settings["server"]["preview"]),
+    }
+
+    return settings
+
 #endregion
 
 #region commands
 
-def preview(anon: AnonPy, args: Namespace, config: ConfigHandler) -> None:
-    verbose = args.verbose or config.get_option("client", "verbose")
+def preview(anon: AnonPy, args: Namespace, settings: EvalConfig) -> None:
+    client = settings["client"]
 
     for resource in args.resource:
         with console.status("fetching data...") as _:
             preview = anon.preview(resource)
-            console.print(JSON(json.dumps(preview)) if verbose else ",".join(preview.values()))
+            console.print(JSON(json.dumps(preview)) if client["verbose"] else ",".join(preview.values()))
 
-def upload(anon: AnonPy, args: Namespace, config: ConfigHandler) -> None:
-    verbose = args.verbose or config.get_option("client", "verbose")
+def upload(anon: AnonPy, args: Namespace, settings: EvalConfig) -> None:
+    client = settings["client"]
 
     for file in args.file:
-        upload = anon.upload(file, enable_progressbar=verbose)
+        upload = anon.upload(file, enable_progressbar=client["verbose"])
         url = upload["url"]
         anon.logger.info("Uploaded %s to %s" % (file, url))
         console.print(f"URL={url}")
 
         if args.clip: copy_to_clipboard(url)
 
-        if not verbose: continue
+        if not client["verbose"]: continue
         computed_hash = Checksum.compute(path=file, algorithm=MD5)
         checksum = Checksum.hash2string(computed_hash)
         console.print(f"MD5=[bold blue]{checksum}[/]")
 
-def download(anon: AnonPy, args: Namespace, config: ConfigHandler) -> None:
-    download_directory = Path(getattr(args, "path", config.get_option("client", "download_directory")))
-    verbose = args.verbose or config.get_option("client", "verbose")
-    force = args.force or config.get_option("client", "force")
+def download(anon: AnonPy, args: Namespace, settings: EvalConfig) -> None:
+    client = settings["client"]
 
     for resource in (args.resource or read_file(args.batch_file)):
         with console.status("fetching data...") as _:
@@ -88,21 +123,21 @@ def download(anon: AnonPy, args: Namespace, config: ConfigHandler) -> None:
             name = preview["name"]
             size = int(preview["size"])
 
-        full_path = download_directory / name
+        full_path = client["download_directory"] / name
 
-        if not force and full_path.exists():
+        if not client["force"] and full_path.exists():
             console.print(f"[bold yellow]WARNING:[/] The file [bold blue]{str(full_path)}[/] already exists")
             prompt = console.input("Proceed with download? [dim][Y/n][/] ")
             if not str2bool(prompt): continue
 
-        anon.download(resource, download_directory, enable_progressbar=verbose, size=size, name=name)
+        anon.download(resource, client["download_directory"], enable_progressbar=client["verbose"], size=size, name=name)
         console.print(f"PATH=[bold blue]{str(full_path)}[/]")
 
         if getattr(args, "checksum", None) is None: continue
         computed_hash = Checksum.compute(path=name, algorithm=MD5)
         computed_checksum = Checksum.hash2string(computed_hash)
 
-        if verbose: console.print(f"MD5={computed_checksum}")
+        if client["verbose"]: console.print(f"MD5={computed_checksum}")
 
         expected_checksum = args.checksum
         corrupt = computed_checksum.lower() != expected_checksum.lower()
@@ -122,9 +157,7 @@ def _start(module_folder: Path, cfg_file: str) -> ArgumentParser:
 
     # Initialize default settings
     cfg_path = module_folder / cfg_file
-
-    if not cfg_path.exists():
-        init_settings(cfg_path)
+    if not cfg_path.exists(): init_config(cfg_path)
 
     return parser
 
@@ -139,19 +172,16 @@ def main() -> None:
     config = ConfigHandler(getattr(args, "config", module_folder / cfg_file))
     config.read()
 
+    settings = eval_config(args, config)
     kwargs = {
-        "api": config.get_option("server", "api"),
-        "endpoint": Endpoint(config.get_option("server", "upload"), config.get_option("server", "download"), config.get_option("server", "preview")),
-        "token": getattr(args, "token", config.get_option("client", "token")),
-        "proxies": getattr(args, "proxies", config.get_option("client", "proxies")),
-        "user_agent": getattr(args, "user_agent", config.get_option("client", "user_agent")) or RequestHandler.build_user_agent(__package__, __version__),
-        "enable_logging": args.logging or config.get_option("client", "enable_logging"),
-    }
+        k: v for k, v in settings["client"].items()
+        if k in ["api", "token", "proxies", "user_agent", "enable_logging"]
+    } | settings["object"] | {"api": settings["server"]["api"]}
 
     provider = AnonPy(**kwargs)
     provider.logger \
         .set_base_path(module_folder) \
-        .with_level(LogLevel(config.get_option("client", "log_level"))) \
+        .with_level(LogLevel(settings["client"]["log_level"])) \
         .add_handler(log_file)
 
     if args.gui:
@@ -160,17 +190,17 @@ def main() -> None:
 
     if args.reset_config:
         config.path.unlink(missing_ok=True)
-        init_settings(config.path)
+        init_config(config.path)
         return
 
     try:
         match args.command:
             case "preview":
-                preview(provider, args, config)
+                preview(provider, args, settings)
             case "upload":
-                upload(provider, args, config)
+                upload(provider, args, settings)
             case "download":
-                download(provider, args, config)
+                download(provider, args, settings)
             case _:
                 # argparse prints the help manual and exits if there are any
                 # errors on parsing, so there's no need to handle this case
